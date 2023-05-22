3eb9c8417181a4dae94fce566b4638c3287c3b57
Adrian Niezgoda <adrian01234@gazeta.pl>
2023-05-22 19:02:49 +0200

    Add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+{
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
@@ -49,3 +49,46 @@ const links = document.querySelectorAll('.titles a');
  //   const links = document.querySelectorAll('.titles a');
   //  console.log('links:', links);
  // }); 
+
+ const optArticleSelector = '.post',
+ optTitleSelector = '.post-title',
+ optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+ /* remove contents of titleList */
+ 
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+ /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html ='';
+
+    for(let article of articles){
+    
+   /* get the article id */
+   const articleId = article.getAttribute("id");
+
+   /* find the title element */
+   const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+   /* get the title from the title element */
+   const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+   console.log(linkHTML);
+   /* create HTML of the link */
+   titleList.innerHTML = titleList.innerHTML + linkHTML;
+  
+   /* insert link into titleList */
+   html = html + linkHTML;
+   console.log(html);
+  }
+  titleList.innerHTML = html;
+
+}
+
+generateTitleLinks();
+
+
+
+}
