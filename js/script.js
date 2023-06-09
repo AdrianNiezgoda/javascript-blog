0438632423ef6cc14756642492342e2ece3cdf9d
Adrian Niezgoda <adrian01234@gazeta.pl>
2023-06-09 17:12:07 +0200

    Add arrays allTags

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-    //console.log(this);
-    //console.log(event);
   
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -13,10 +11,7 @@
       activeLink.classList.remove('active');
     }
     /* [DONE] add class 'active' to the clicked link */
-  
     clickedElement.classList.add('active');
-
-    //console.log('clickedElement (with plus): ' + clickedElement);
   
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post');
@@ -25,26 +20,15 @@
       activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-
     const articleSelector = clickedElement.getAttribute('href');
-    //console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
-
     const targetArticle = document.querySelector(articleSelector);
-    //console.log(targetArticle);
   
     /* add class 'active' to the correct article */
-
     targetArticle.classList.add('active');
 
   };
-  
-
-  //document.getElementById('test-button').addEventListener('click', function(){
-  //   const links = document.querySelectorAll('.titles a');
-  //  console.log('links:', links);
-  // }); 
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
@@ -52,8 +36,6 @@
     optArticleAuthorSelector = '.post-author';
 
     function generateTitleLinks(customSelector = ''){
-  //console.log(customSelector);
-  //console.log(optArticleSelector);
     /* remove contents of titleList */
  
     const titleList = document.querySelector(optTitleListSelector);
@@ -73,18 +55,15 @@
 
       /* get the title from the title element */
       const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-      //console.log(linkHTML);
       /* create HTML of the link */
       titleList.innerHTML = titleList.innerHTML + linkHTML;
   
       /* insert link into titleList */
       html = html + linkHTML;
-      //console.log(html);
     }
     titleList.innerHTML = html;
 
     const links = document.querySelectorAll('.titles a');
-    //console.log(links);
     for(let link of links){
       link.addEventListener('click', titleClickHandler);
     }
@@ -95,6 +74,9 @@
   function generateTags(){
     
     const optArticleTagsSelector = '.post-tags .list';
+    const optTagsListSelector = '.tags.list';
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
     /* find all articles */
     const articles = document.querySelectorAll('article');
     /* START LOOP: for every article: */
@@ -105,23 +87,29 @@
       let html = '';
       /* get tags from data-tags attribute */
       const articleTags = article.getAttribute('data-tags');
-      //console.log(articleTags);
       /* split tags into array */
       const articleTagsArray = articleTags.split(' ');
-      //console.log(articleTagsArray);
       /* START LOOP: for each tag */
       for(let tag of articleTagsArray){
         /* generate HTML of the link */
         const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-        //console.log (linkHTML);
         /* add generated code to html variable */
         html = html +linkHTML+'';
+        /* [NEW] check if this link is NOT already in allTags */
+        if(allTags.indexOf(linkHTML) == -1){
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
       /* END LOOP: for each tag */
       }
       /* insert HTML of all the links into the tags wrapper */
       tagsList.innerHTML = html;
     /* END LOOP: for every article: */
     } 
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
   }
     
   
@@ -208,5 +196,5 @@
     }
     generateTitleLinks('[data-author="' + author + '"]');
   }
-  // Nie jestem pewny rozwiązania i też nie rozumiem dlaczego po kliknięciu w tag lub autora czyści mi listę arykułów 
+  
 }
